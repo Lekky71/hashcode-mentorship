aeab658a92d4ccf3625f5b8a965789f25d792ca8
Fakorede Oluwaleke <hashilekky@gmail.com>
2023-03-05 11:08:33 -0500

    4th meeting

---
+++ assignments/day4.js
@@ -0,0 +1,5 @@
+// Write a Javascript function that receives your names as parameter and then returns your surname and initials.
+// For example: Fakorede Oluwaleke Adedoyin => Fakorede 0.A.
+// For example: Ojo Boluwatife Fiyinfoluwa => Ojo B.F.
+// For example: Ojo Tunde-Lawal Boluwatife John => Tunde-Lawal B.J.
+// For example: Ojo Fakolade Liberty Semilore => Fakolade F.S.
